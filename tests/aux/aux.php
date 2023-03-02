bc3a70dd378ae7a3439ca6f7ae46a193b1f6e0dd
Ondřej Koupil <ondra@ondrakoupil.cz>
2023-03-02 08:59:13 +0100

    Adressing problems with aux/aux.php on windows

--- tests/aux/aux.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include __DIR__ . '/ArrayAccessTestObject.php';
-include __DIR__ . '/TraversableTestObject.php';
-include __DIR__ . '/FilesTestCase.php';
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
-require __DIR__ . "/../vendor/autoload.php";
-require __DIR__ . '/aux/aux.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/aux/ArrayAccessTestObject.php';
+require __DIR__ . '/aux/FilesTestCase.php';
+require __DIR__ . '/aux/TraversableTestObject.php';
 
-define("TMP_TEST_DIR", __DIR__ . "/temp");
+define('TMP_TEST_DIR', __DIR__ . '/temp');
 
 \Tester\Environment::setup();
